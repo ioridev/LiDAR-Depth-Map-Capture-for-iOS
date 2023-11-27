65053e1bc63b51dbea0a7ace344fccff89042871
iori kojima <iori@iori.dev>
2023-11-27 15:51:56 +0900

    Initial Commit

---
+++ DepthCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DepthCamera
+//
+//  Created by iori on 2023/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DepthCamera/DepthCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepthCameraApp.swift
+//  DepthCamera
+//
+//  Created by iori on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct DepthCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
