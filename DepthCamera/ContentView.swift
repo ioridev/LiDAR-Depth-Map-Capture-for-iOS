f91178055e0a56a559351ce37f52426dc9f7742b
Iori Kojima <iori@iori.dev>
2024-04-29 14:47:48 +0900

    indent

--- DepthCamera/ContentView.swift
+++ DepthCamera/ContentView.swift
@@ -12,26 +12,26 @@ import tiff_ios
 struct ContentView : View {
     @StateObject var arViewModel = ARViewModel()
     let previewCornerRadius: CGFloat = 15.0
-
+    
     var body: some View {
         
         GeometryReader { geometry in
             ZStack {
-                        // Make the entire background black.
-                        Color.black.edgesIgnoringSafeArea(.all)
-        VStack {
-
-                             let width = geometry.size.width
-                             let height = width * 4 / 3 // 4:3 aspect ratio
-                             ARViewContainer(arViewModel: arViewModel)
-                                 .clipShape(RoundedRectangle(cornerRadius: previewCornerRadius))
-                                 .frame(width: width, height: height)
-            CaptureButtonPanelView(model: arViewModel,  width: geometry.size.width)
-
-                         }
+                // Make the entire background black.
+                Color.black.edgesIgnoringSafeArea(.all)
+                VStack {
+                    
+                    let width = geometry.size.width
+                    let height = width * 4 / 3 // 4:3 aspect ratio
+                    ARViewContainer(arViewModel: arViewModel)
+                        .clipShape(RoundedRectangle(cornerRadius: previewCornerRadius))
+                        .frame(width: width, height: height)
+                    CaptureButtonPanelView(model: arViewModel,  width: geometry.size.width)
+                    
+                }
+            }
         }
-        }
-                 .environment(\.colorScheme, .dark)
+        .environment(\.colorScheme, .dark)
     }
 }
 
@@ -39,43 +39,43 @@ struct ARViewContainer: UIViewRepresentable {
     @ObservedObject var arViewModel: ARViewModel
     
     func find4by3VideoFormat() -> ARConfiguration.VideoFormat? {
-           let availableFormats = ARWorldTrackingConfiguration.supportedVideoFormats
-           for format in availableFormats {
-               let resolution = format.imageResolution
-               if resolution.width / 4 == resolution.height / 3 {
-                   print("Using video format: \(format)")
-                   return format
-               }
-           }
-           return nil
-       }
-
-
-
+        let availableFormats = ARWorldTrackingConfiguration.supportedVideoFormats
+        for format in availableFormats {
+            let resolution = format.imageResolution
+            if resolution.width / 4 == resolution.height / 3 {
+                print("Using video format: \(format)")
+                return format
+            }
+        }
+        return nil
+    }
+    
+    
+    
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
-
+        
         let configuration = ARWorldTrackingConfiguration()
         configuration.sceneReconstruction = .meshWithClassification
-
+        
         if ARWorldTrackingConfiguration.supportsSceneReconstruction(.meshWithClassification) {
             configuration.sceneReconstruction = .meshWithClassification
         }
-
+        
         configuration.frameSemantics.insert(.sceneDepth)
         if let format = find4by3VideoFormat() {
-                  configuration.videoFormat = format
-              } else {
-                  print("No 4:3 video format is available")
-              }
-
-
+            configuration.videoFormat = format
+        } else {
+            print("No 4:3 video format is available")
+        }
+        
+        
         arView.session.delegate = arViewModel
         arView.session.run(configuration)
-
+        
         return arView
     }
-
+    
     func updateUIView(_ uiView: ARView, context: Context) {}
 }
 
@@ -83,61 +83,61 @@ class ARViewModel: NSObject, ARSessionDelegate, ObservableObject {
     private var latestDepthMap: CVPixelBuffer?
     private var latestImage: CVPixelBuffer?
     @Published var lastCapture: UIImage? = nil {
-          didSet {
-              print("lastCapture was set.")
-          }
-      }
-
+        didSet {
+            print("lastCapture was set.")
+        }
+    }
+    
     
     
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         latestDepthMap = frame.sceneDepth?.depthMap
         latestImage = frame.capturedImage
-
-    }
-    
-func saveDepthMap() {
-    guard let depthMap = latestDepthMap, let image = latestImage else {
-        print("Depth map or image is not available.")
-        return
-    }
-    
-    let documentsDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyyMMdd"
-    let dateString = dateFormatter.string(from: Date())
-    let dateDirURL = documentsDir.appendingPathComponent(dateString)
-    
-    do {
-        try FileManager.default.createDirectory(at: dateDirURL, withIntermediateDirectories: true, attributes: nil)
-    } catch {
-        print("Failed to create directory: \(error)")
-        return
-    }
-    
-    let timestamp = Date().timeIntervalSince1970
-    let depthFileURL = dateDirURL.appendingPathComponent("\(timestamp)_depth.tiff")
-    let imageFileURL = dateDirURL.appendingPathComponent("\(timestamp)_image.jpg")
-    
-    writeDepthMapToTIFFWithLibTIFF(depthMap: depthMap, url: depthFileURL)
-    saveImage(image: image, url: imageFileURL)
-    
-    
-    
-
-
-    let uiImage = UIImage(ciImage: CIImage(cvPixelBuffer: image))
-
-    
-    DispatchQueue.main.async {
-                      self.lastCapture = uiImage
-                  }
-    
         
+    }
     
-    print("Depth map saved to \(depthFileURL)")
-    print("Image saved to \(imageFileURL)")
-}
+    func saveDepthMap() {
+        guard let depthMap = latestDepthMap, let image = latestImage else {
+            print("Depth map or image is not available.")
+            return
+        }
+        
+        let documentsDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        let dateString = dateFormatter.string(from: Date())
+        let dateDirURL = documentsDir.appendingPathComponent(dateString)
+        
+        do {
+            try FileManager.default.createDirectory(at: dateDirURL, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            print("Failed to create directory: \(error)")
+            return
+        }
+        
+        let timestamp = Date().timeIntervalSince1970
+        let depthFileURL = dateDirURL.appendingPathComponent("\(timestamp)_depth.tiff")
+        let imageFileURL = dateDirURL.appendingPathComponent("\(timestamp)_image.jpg")
+        
+        writeDepthMapToTIFFWithLibTIFF(depthMap: depthMap, url: depthFileURL)
+        saveImage(image: image, url: imageFileURL)
+        
+        
+        
+        
+        
+        let uiImage = UIImage(ciImage: CIImage(cvPixelBuffer: image))
+        
+        
+        DispatchQueue.main.async {
+            self.lastCapture = uiImage
+        }
+        
+        
+        
+        print("Depth map saved to \(depthFileURL)")
+        print("Image saved to \(imageFileURL)")
+    }
 }
 
 
@@ -146,19 +146,19 @@ func saveDepthMap() {
 func writeDepthMapToTIFFWithLibTIFF(depthMap: CVPixelBuffer, url: URL) -> Bool {
     let width = CVPixelBufferGetWidth(depthMap)
     let height = CVPixelBufferGetHeight(depthMap)
-
+    
     CVPixelBufferLockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 0))
     guard let baseAddress = CVPixelBufferGetBaseAddress(depthMap) else {
         CVPixelBufferUnlockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 0))
         return false
     }
     let bytesPerRow = CVPixelBufferGetBytesPerRow(depthMap)
-
+    
     guard let rasters = TIFFRasters(width: Int32(width), andHeight: Int32(height), andSamplesPerPixel: 1, andSingleBitsPerSample: 32) else {
         CVPixelBufferUnlockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 0))
         return false
     }
-
+    
     for y in 0..<height {
         let pixelBytes = baseAddress.advanced(by: y * bytesPerRow)
         let pixelBuffer = UnsafeBufferPointer<Float>(start: pixelBytes.assumingMemoryBound(to: Float.self), count: width)
@@ -166,11 +166,11 @@ func writeDepthMapToTIFFWithLibTIFF(depthMap: CVPixelBuffer, url: URL) -> Bool {
             rasters.setFirstPixelSampleAtX(Int32(x), andY: Int32(y), withValue: NSDecimalNumber(value: pixelBuffer[x]))
         }
     }
-
+    
     CVPixelBufferUnlockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 0))
-
+    
     let rowsPerStrip = UInt16(rasters.calculateRowsPerStrip(withPlanarConfiguration: Int32(TIFF_PLANAR_CONFIGURATION_CHUNKY)))
-
+    
     guard let directory = TIFFFileDirectory() else {
         return false
     }
@@ -184,14 +184,14 @@ func writeDepthMapToTIFFWithLibTIFF(depthMap: CVPixelBuffer, url: URL) -> Bool {
     directory.setPlanarConfiguration(UInt16(TIFF_PLANAR_CONFIGURATION_CHUNKY))
     directory.setSampleFormatAsSingleValue(UInt16(TIFF_SAMPLE_FORMAT_FLOAT))
     directory.writeRasters = rasters
-
+    
     guard let tiffImage = TIFFImage() else {
         return false
     }
     tiffImage.addFileDirectory(directory)
-
+    
     TIFFWriter.writeTiff(withFile: url.path, andImage: tiffImage)
-
+    
     return true
 }
 
@@ -217,11 +217,11 @@ struct CaptureButtonPanelView: View {
     @Environment(\.presentationMode) var presentationMode
     @State private var showAlert = false // State variable to control alert visibility
     
- 
-
-
-
-
+    
+    
+    
+    
+    
     var body: some View {
         ZStack(alignment: .bottomLeading) {
             HStack {
@@ -238,37 +238,37 @@ struct CaptureButtonPanelView: View {
                 Spacer()
             }
             HStack {
-                  /*
-              
-                                Spacer()
-              
-                Spacer()
-                Button( action: {
-                      
-                        }) {
-                    Text("")
-                                .font(.system(size: 14)).bold()
-
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 60 , height: 60)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.white, lineWidth: 2)
-                        )
+                /*
                  
-                .padding(.horizontal)
+                 Spacer()
+                 
+                 Spacer()
+                 Button( action: {
+                 
+                 }) {
+                 Text("")
+                 .font(.system(size: 14)).bold()
+                 
+                 .foregroundColor(.white)
+                 .padding()
+                 .frame(width: 60 , height: 60)
+                 .overlay(
+                 RoundedRectangle(cornerRadius: 10)
+                 .stroke(Color.white, lineWidth: 2)
+                 )
+                 
+                 .padding(.horizontal)
+                 }
+                 
+                 */
+                
             }
-
-              */
-                 
         }
     }
-}
     
+    
+}
 
-}
-    
 
 
 
@@ -283,14 +283,14 @@ struct ThumbnailView: View {
     private let thumbnailFrameCornerRadius: CGFloat = 10.0
     private let thumbnailStrokeWidth: CGFloat = 2
     
-
+    
     
     @ObservedObject var model: ARViewModel
     @Environment(\.presentationMode) var presentationMode
-
+    
     var body: some View {
         Button(action: {
-
+            
         }) {
             Group {
                 if let capture = model.lastCapture {
@@ -350,7 +350,7 @@ struct ThumbnailImageView: View {
             .cornerRadius(thumbnailFrameCornerRadius)
             .clipped()
             .overlay(RoundedRectangle(cornerRadius: thumbnailFrameCornerRadius)
-                        .stroke(Color.primary, lineWidth: thumbnailStrokeWidth))
+                .stroke(Color.primary, lineWidth: thumbnailStrokeWidth))
             .shadow(radius: 10)
     }
 }
@@ -368,7 +368,7 @@ struct CaptureButton: View {
     CaptureButton.innerPadding
     
     @ObservedObject var model: ARViewModel
-
+    
     init(model: ARViewModel) {
         self.model = model
     }
@@ -379,8 +379,8 @@ struct CaptureButton: View {
             model.saveDepthMap()
         },label: {
             
-                ManualCaptureButtonView()
-        
+            ManualCaptureButtonView()
+            
         })
     }
 }
